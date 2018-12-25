714d66e56b8266de46295704c916ff1a8780dedb
Timothy Robinson <timothyr218@gmail.com>
2018-12-25 10:27:46 -0800

    Created TypeData ScriptableObject Script
    Created the TypeData ScriptableObject Script to hold the data for any new Types and what types they would resist or be effective against.

---
+++ Assets/TypeData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Type", menuName = "Type")]
+public class TypeData : ScriptableObject
+{
+    public string type_name;
+    public List<string> Effective = new List<string>();
+    public List<string> Resists = new List<string>();
+}

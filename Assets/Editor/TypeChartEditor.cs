8b76f9cf64d30d4b5dbb04f91ff3908a80c1911c
Timothy Robinson <timothyr218@gmail.com>
2018-12-28 13:57:05 -0800

    Started TypeChartEditor Script
    - Created TypeChartEditor Script
    - Currently Creates a Custom Window under Window / TypeChart

---
+++ Assets/Editor/TypeChartEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TypeChartEditor : EditorWindow
+{
+    [MenuItem("Window/TypeChart")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(TypeChartEditor), false, "Type Chart");
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}

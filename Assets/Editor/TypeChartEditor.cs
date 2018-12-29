c6187d1e952e8a87b80acba5bd52572e2839ae5c
Timothy Robinson <timothyr218@gmail.com>
2018-12-28 23:35:24 -0800

    TypeChartEditor Script Code Testing
    - Testing Code in TypeChartEditor

--- Assets/Editor/TypeChartEditor.cs
+++ Assets/Editor/TypeChartEditor.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class TypeChartEditor : EditorWindow
 {
+    private string typeField;
+    private string effectField;
+
     [MenuItem("Window/TypeChart")]
     public static void ShowWindow()
     {
@@ -13,6 +16,20 @@ public class TypeChartEditor : EditorWindow
 
     private void OnGUI()
     {
-        
+        GUIStyle textStyle = new GUIStyle
+        {
+            //fontSize = 10
+            //alignment = TextAnchor.MiddleCenter
+            border = new RectOffset(0, 50, 0, -50)
+        };
+
+        EditorGUILayout.BeginVertical();
+            EditorGUILayout.BeginHorizontal();
+                typeField = EditorGUILayout.TextField(typeField, GUILayout.Width(50), GUILayout.Height(50), 
+                    GUILayout.ExpandHeight(false),GUILayout.ExpandWidth(false));
+                effectField = EditorGUILayout.TextField(effectField, GUILayout.Width(50), GUILayout.Height(50),
+                    GUILayout.ExpandHeight(false), GUILayout.ExpandWidth(false));
+            EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
     }
 }

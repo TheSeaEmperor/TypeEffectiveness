c8eaf04b896d5c51654468d2ac4f0476cf1c01e0
Timothy Robinson <timothyr218@gmail.com>
2019-01-10 15:03:23 -0800

    Added Damage Calc Function
    - The function takes in the base damage, the attacking Type and the Defending Type
    - It checks whether to divide, multiply, or do normal damage based on Effectiveness and Resistance
    - Returns the proper damage as result

--- Assets/Scripts/DamageCalc.cs
+++ Assets/Scripts/DamageCalc.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public static class DamageCalc
 {
-    
+    public static float CalcDamage(float damage, TypeData attack, TypeData defend)
+    {
+        for (int i = 0; i < defend.Effective.Count; i++)
+        {
+            if (defend.Effective[i].Equals(attack.type_name))
+                return damage * 2;
+        }
+        for (int i = 0; i < defend.Resists.Count; i++)
+        {
+            if (defend.Resists[i].Equals(attack.type_name))
+                return damage / 2;
+        }
+
+        return damage;
+    }
 }

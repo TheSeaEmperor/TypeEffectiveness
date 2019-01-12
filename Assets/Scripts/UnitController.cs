c3bd849e5b3992e89df416d9b06b9ba615abd455
Timothy Robinson <timothyr218@gmail.com>
2019-01-12 14:11:02 -0800

    Created UnitController Script
    - Created UnitController Script for testing out the type system.

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public int health;
+    public int base_damage;
+    public TypeData type;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            DamageCalc.CalcDamage(base_damage, type, other.GetComponent<UnitController>().type);
+        }
+    }
+}
